4a58a4aa9b1319e8c0afacd686c5431a81e42816
Alex Merecka <merecka@gmail.com>
2019-04-12 17:50:05 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,30 @@
 class Owner
-  # code goes here
+
+  attr_accessor :owner_name
+
+  @@all = []
+
+  #Class methods
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  #Instance Methods
+
+  def initialize(owner_name)
+    @owner_name = owner_name
+    @@all << self
+  end
+
+
+
 end
